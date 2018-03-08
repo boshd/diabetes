cfc81f09cb2c5b4bb68f81e58805ce2c218e2447
Kareem Arab <kareem@5odbalak.com>
2018-03-08 12:10:12 -0500

    test

--- analytics.py
+++ analytics.py
@@ -1,3 +1,9 @@
-def analyze(data):
+import tensorflow as tf
+
+def analyze():
+	hello = tf.constant('Hello, TensorFlow!')
+	sess = tf.Session()
+	return sess.run(hello)
+
+def anomaly_detection():
 	pass
-		
--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import prepare as p
 import pre_process as pp
-import analytics as an
-import metrics as metr
+import analytics as a
+import metrics as m
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # disables TF GPU warnings
 
 def main():
     
 	# Prepare
-	x = p.prep();
+	x = p.prep()
 	print(x)
 	# Pre-Process
 
 	# Analytics
-
+	y = a.analyze()
+	print(y)
 	# Metrics
 
 	# Random Stuff
-
-	pass
 
 
 if __name__ == '__main__':
