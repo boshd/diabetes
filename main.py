b43ef60973ef969067a16cc4578709057e059c2e
Kareem Arab <kareem@5odbalak.com>
2018-03-29 16:07:54 -0400

    hala wala

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from os import listdir
 import pre_process as pre_process
 import analytics as analytics
 import prepare as prepare
-import metrics.metrics as metrics
+import metrics.confidence as conf
 import visualize as visualize
 import numpy as np
 
@@ -17,10 +17,10 @@ def main():
 	files = [f for f in listdir("./datasets/data/") if isfile(join("./datasets/data", f)) and not f.startswith('.')]
 	sorted_files = sorted(files, key=lambda item: (int(item.partition(' ')[0]) 
 		if item[0].isdigit() else float('inf'), item))
-	
+
 	#prepared_data = prepare.prepare(files, 43, 57)
 	
-	array_with_all_len_metrics = metrics.data_confidence(sorted_files, 60)
+	array_with_all_len_metrics = conf.data_confidence(sorted_files, 60)
 	print(array_with_all_len_metrics)
 	
 	#visualize.visualize(prepared_data)
---
+++ metrics/eval_metrics.py
@@ -0,0 +1,49 @@
+import os, random, math, scipy
+import pandas as pd
+import numpy as np
+
+def rmse():
+	pass
+
+def mse():
+	pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
